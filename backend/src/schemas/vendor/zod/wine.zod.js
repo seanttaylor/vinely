c3e8444836aa8bc79444645757f065b23c8b9fa7
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2026-04-02 17:42:27 +0000

    ADDED NORMALIZATION OF EXTRACTED CSV WINE RECORDS

---
+++ backend/src/schemas/vendor/zod/wine.zod.js
@@ -0,0 +1,74 @@
+import { format } from "morgan";
+import { z } from "zod";
+
+/**
+ * @description Zod schema used to convert input objects to validated strongly-typed objects;
+ * used in the context of bulk imports. We want to guarantee the structure of imported
+ * records prior to executing further processing
+ * @returns {Object} a value object representing data about a single wine
+ */
+const WineZodSchema = z.object({
+  id: z.string().uuid(),
+
+  created_at: z.string(), // keep as string; DB can cast to timestamptz
+
+  name: z.string().min(1).transform(s => s.trim()),
+
+  color: z.string()
+    .min(1)
+    .transform(s => s.trim().toLowerCase()),
+
+  sparkling: z.coerce.boolean(),
+
+  minerality: z.coerce.number(),
+
+  description: z.string()
+    .min(1)
+    .transform(s => s.trim()),
+
+  acidity: z.coerce.number(),
+
+  body: z.string()
+    .min(1)
+    .transform(s => s.trim().toLowerCase()),
+
+  tags: z.string()
+    .optional()
+    .transform(s => {
+      if (!s || s.trim() === "") return null;
+      return s.trim();
+    }),
+
+  vintage: z.string()
+    .min(1)
+    .transform(s => s.trim()),
+
+  tasting_notes: z.string()
+    .optional()
+    .transform(s => {
+      if (!s || s.trim() === "") return null;
+      return s.trim();
+    }),
+
+  producer_id: z.string().uuid(),
+
+  price: z.coerce.number(),
+
+  sweetness: z.coerce.number(),
+
+  kosher: z.coerce.boolean().optional(),
+
+  name_tsv: z.string().optional().nullable(),
+});
+
+
+/**
+ * Factory for generating Wine value objects
+ */
+export const Wine = (() => ({
+    from(input) {
+        return WineZodSchema.parse(input)
+    }
+}))();
+
+
--- backend/src/services/task-provider/task-provider.hc.js
+++ backend/src/services/task-provider/task-provider.hc.js
@@ -1,6 +1,7 @@
 import { parse } from "csv-parse/sync";
-import { Result } from "../types/result.js";
-import { ApplicationService } from "../../system.js";
+import { Result } from "../../types/result.js";
+import { ApplicationService } from "../../../system.js";
+import { Wine } from "../../schemas/vendor/zod/wine.zod.js";
 
 /**
  * @description Configuration for CSV Parse
@@ -9,6 +10,17 @@ const CONFIG = {
   columns: true,
   skip_empty_lines: true,
 };
+
+/**
+ * Post-processing step for bulk importing wines via CSV file for example. Since
+ * CSV extraction casts all values as strings we must re-cast field values to
+ * the correct types for downstream processing.
+ * @param {object[]} input - list of wine records 
+ * @returns {object[]}
+ */
+const normalizeWines = (input) => {
+  return input.map(Wine.from); 
+}
 
 /**
  * Houses implementation details of all supported system tasks
@@ -42,12 +54,17 @@ export default class TaskProvider extends ApplicationService {
      * @param {TaskHandle}
      */
     "tasks.test.noop": async (input, signal, taskHandle) => {
+      try {
         this.#logger.log(`running task (${taskHandle.name}) as instance (${taskHandle.instance})`);
-        this.#logger.log({input});
 
         const csvString = input.file.buffer.toString("UTF-8");
         const records = parse(csvString, CONFIG);
-        Result.ok(records).map(/* so much stuff */);
+        const normalizedResult = Result.ok(records)
+        .map(normalizeWines)
+      } catch(ex) {
+        this.#logger.error(`INTERNAL ERROR (TaskProvider): **EXCEPTION ENCOUNTERED** while running task (${taskHandle.name}) as instance (${taskHandle.instance}). Task will be **STOPPED** See details -> ${ex.message}`);
+        taskHandle.stop(ex.message);
+      }
     }
   }
 }
